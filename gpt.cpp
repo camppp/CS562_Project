190bfab79a4d1277e932cdf031d1d6b3168f73a3
Xianrui Zhong <xzhong23@illinois.edu>
2023-12-10 15:04:45 -0600

    test gpt

--- gpt.cpp
+++ gpt.cpp
@@ -1,7 +1,20 @@
-int sumArray(int arr[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += arr[i];
+#include <stdio.h>
+#include <stdlib.h>
+
+int* process_packets(int** packets, int num_packets, int* packet_sizes) {
+    int *sum_array = (int*)malloc(num_packets * sizeof(int));
+    
+    if(sum_array == NULL) {
+        printf("Memory allocation failed\n");
+        return NULL;
     }
-    return sum;
+    
+    for(int i = 0; i < num_packets; i++) {
+        int sum = 0;
+        for(int j = 0; j < packet_sizes[i]; j++) {
+            sum += packets[i][j];
+        }
+        sum_array[i] = sum;
+    }
+    return sum_array;
 }
