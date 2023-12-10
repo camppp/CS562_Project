7b9f3c76ac52be050ae0bab891af0379652f2024
Xianrui Zhong <xzhong23@illinois.edu>
2023-12-10 14:32:01 -0600

    test codellama

--- codellama.cpp
+++ codellama.cpp
@@ -1,18 +1,7 @@
-#include <iostream>
-
-int main() {
-    int arr[5];
-    int i;
-
-    std::cout << "Enter 5 values: ";
-    for (i = 0; i < 5; i++) {
-        std::cin >> arr[i];
+int sumArray(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += arr[i];
     }
-
-    // Print the array
-    for (i = 0; i < 5; i++) {
-        std::cout << arr[i] << " ";
-    }
-
-    return 0;
+    return sum;
 }
