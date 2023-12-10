c776302e145b8ee47b13f3b3dda4e7c515958392
Xianrui Zhong <xzhong23@illinois.edu>
2023-12-10 15:03:51 -0600

    test llama

--- codellama.cpp
+++ codellama.cpp
@@ -1,6 +1,11 @@
-#include <string>
-
-string rotateString(string s, int n) {
-    // Rotate the string to the right by n positions
-    return s.substr(n) + s.substr(0, n);
+int* process_packets(int** packets, int num_packets, int* packet_sizes) {
+    int* sums = (int*)malloc(num_packets * sizeof(int));
+    for (int i = 0; i < num_packets; i++) {
+        int sum = 0;
+        for (int j = 0; j < packet_sizes[i]; j++) {
+            sum += packets[i][j];
+        }
+        sums[i] = sum;
+    }
+    return sums;
 }
